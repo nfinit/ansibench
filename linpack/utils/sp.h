0e9bc692f017bb6db09203f449c125d90ad19838
ict <ict@nfinit.systems>
2019-06-13 10:29:12 -0600

    Added single-precision CodeWarrior prefix file
    Added simple header file to define SP at compile time to allow for switchable precision in the CodeWarrior development environment without modifying main source.

---
+++ linpack/utils/sp.h
@@ -0,0 +1,6 @@
+/* Simple header file to define single-precision symbol
+ * at compile time for easier CodeWarrior builds. Use
+ * this as the prefix file in your single-precision build targets.
+ */
+
+#define SP
